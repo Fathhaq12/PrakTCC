98d88560f14a178b885a861f886db17935bd8249
fathhaq12 <fathahillahhaq@gmail.com>
2025-03-17 22:02:53 +0700

    update

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('catatanhq','root','',{
-    host: '34.27.28.164',
-    dialect: 'mysql',});
+const db = new Sequelize("catatanhq", "root", "", {
+  host: "34.27.28.164",
+  dialect: "mysql",
+});
 
-    export default db;
+export default db;
